f075d49cbb78b37ef99f9654355e38be49ce5f18
Emma Heikkinen <emhe2504@student.miun.se>
2026-05-23 14:27:02 +0200

    Skapade meny

--- src/main.js
+++ src/main.js
@@ -1,1 +1,79 @@
+const starters = document.getElementById("starters");
+const mainMeals = document.getElementById("mainMeals");
+const dessert = document.getElementById("dessert");
+const beverage = document.getElementById("beverage");
 
+window.onload = init;
+
+function init() {
+
+    if (menu) { getMenu() };
+}
+
+//Hämta menu från API
+async function getMenu() {
+
+    try {
+
+        const fetchData = await fetch("http://localhost:3001/menu");
+        const jsonData = await fetchData.json();
+
+        renderMenu(jsonData);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Skriva ut menu till DOM
+function renderMenu(jsonData) {
+
+    starters.innerHTML = "";
+    mainMeals.innerHTML = "";
+    dessert.innerHTML = "";
+    beverage.innerHTML = "";
+
+    jsonData.forEach(meal => {
+        const mealType = meal.mealtype;
+        const mealName = meal.mealname;
+        const mealDescription = meal.mealdescription;
+        const mealPrice = meal.mealprice;
+        
+        if (mealType === "Starter") {
+            starters.innerHTML += `
+        <article>
+        <h3>${mealName}</h3>
+        <p>${mealDescription}</p>
+        <p>${mealPrice}</p>
+        </article>`
+        }
+
+        if (mealType === "Main") {
+            mainMeals.innerHTML += `
+        <article>
+        <h3>${mealName}</h3>
+        <p>${mealDescription}</p>
+        <p>${mealPrice}</p>
+        </article>`
+        }
+
+        if (mealType === "Dessert") {
+            dessert.innerHTML += `
+        <article>
+        <h3>${mealName}</h3>
+        <p>${mealDescription}</p>
+        <p>${mealPrice}</p>
+        </article>`
+        }
+
+        if (mealType === "Beverage") {
+            beverage.innerHTML += `
+        <article>
+        <h3>${mealName}</h3>
+        <p>${mealDescription}</p>
+        <p>${mealPrice}</p>
+        </article>`
+        }
+
+    });
+}

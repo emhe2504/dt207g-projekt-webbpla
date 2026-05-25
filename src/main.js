91fcf2f853f5bc67c0b4406f6023e3dfb07be737
Emma Heikkinen <emhe2504@student.miun.se>
2026-05-25 14:36:48 +0200

    Fixade så anställd kan logga in och token sparas i localStorage

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ const bookingConfirmation = document.getElementById("confirmation");
 const getBookingForm = document.getElementById("getBookingForm");
 const resultSpot = document.getElementById("bookingResult");
 
+const loginForm = document.getElementById("loginForm");
+
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
@@ -20,6 +22,8 @@ function init() {
     if (resultSpot) { resultSpot.classList.add("is_hidden"); }
     if (reservationForm) { reservationForm.addEventListener("submit", addBooking); }
     if (getBookingForm) { getBookingForm.addEventListener("submit", getOneBooking); }
+
+    if (loginForm) { loginForm.addEventListener("submit", login); }
 }
 
 //Hämta menu från API
@@ -226,4 +230,70 @@ async function getOneBooking(event) {
     } catch (error) {
         console.log(error);
     }
+}
+
+//Logga in på registerat konto för anställd
+async function login(event) {
+
+    event.preventDefault(); //Inte ladda om sidan
+
+    //Värden från input
+    const addedEmail = document.getElementById("email").value;
+    const addedPassword = document.getElementById("password").value;
+
+    //Felmeddelanden vid tomma inputfält
+    const errors = [];
+    const errorSpot = document.getElementById("errorUl");
+    errorSpot.innerHTML = "";
+
+    if (!addedEmail) {
+        errors.push("Fyll i E-postadress")
+    }
+    if (!addedPassword) {
+        errors.push("Fyll i lösenord")
+    }
+    errors.forEach(error => {
+        const newLi = document.createElement("li");
+        newLi.textContent = error;
+        errorSpot.appendChild(newLi);
+    });
+
+    let user = {
+        email: addedEmail,
+        password: addedPassword
+    }
+
+    try {
+
+        const response = await fetch("http://localhost:3001/employee/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+
+        const data = await response.json();
+
+        //Felmeddelande om fel epost eller lösen angetts
+        if (!response.ok) {
+            const errorMessage = data.message;
+            const errorSpot = document.getElementById("loginError");
+            errorSpot.textContent = errorMessage;
+            return;
+
+        }
+
+        errorSpot.textContent = "";
+        console.log(data);
+
+        if (data.token) {
+            localStorage.setItem("Employee-token", data.token);
+            window.location.href = "employee.html";
+        }
+
+    } catch (error) {
+    console.log(error);
+}
+
 }
